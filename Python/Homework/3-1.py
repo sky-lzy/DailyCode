cbaa110c1847fef14805f1bd9527c6a8cbff279f
sky-lzy <289751894@qq.com>
2021-07-21 09:55:05 +0800

    master

--- Python/Homework/3-1.py
+++ Python/Homework/3-1.py
@@ -6,10 +6,14 @@ def lock(str, k):
             temp[i] = chr(ord(temp[i]) + k)
             if temp[i] > 'z':
                 temp[i] = chr(ord(temp[i]) - 26)
+            if temp[i] < 'a':
+                temp[i] = chr(ord(temp[i]) + 26)
         elif temp[i] >= 'A' and temp[i] <= 'Z':
             temp[i] = chr(ord(temp[i]) + k)
             if temp[i] > 'Z':
                 temp[i] = chr(ord(temp[i]) - 26)
+            if temp[i] < 'A':
+                temp[i] = chr(ord(temp[i]) + 26)
     str = ''.join(temp)
     return str
 
@@ -20,4 +24,6 @@ str = file.read()
 k = int(input("请输入您需要加密的位数(1~25)："))
 while k < 1 or k > 25:
     k = int(input("请输入正确范围(1~25)："))
-print("加密后的文件信息如下：\n", lock(str, k), sep='', end='')
+lock_str = lock(str, k)
+print("加密后的文件信息如下：\n", lock_str, sep='', end='')
+print("解密后的文件信息如下：\n", lock(lock_str, -k), sep='', end='')
