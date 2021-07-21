ffa4f19e930deb6d3e9f83496167ff6542f147a3
sky-lzy <289751894@qq.com>
2021-07-21 23:06:36 +0800

    master

--- Arduino/Homework/3.ino
+++ Arduino/Homework/3.ino
@@ -1,14 +1,14 @@
 #include <LiquidCrystal.h>
 #include <TimerOne.h>
 #include <String.h>
-#include <SoftwareSerial.h>
+#include <HardwareSerial.h>
 #include <Arduino.h>
 
 //全局变量的定义
 const int rs = 13, en = 12, d7 = 11, d6 = 10, d5 = 9, d4 = 8; //LCD引脚
 const int R = 5, G = 4, B = 3;                                //OLED引脚
 const int Rx = A0, Ry = A1, SW = 2;                           //摇杆引脚
-const int interval = 10000;                                   //10ms刷新一次
+const long interval = 10000;                                  //10ms刷新一次
 unsigned long spend_time = 0;                                 //记录反应时间
 unsigned long set_time = 0;                                   //记录设定时间
 int flag = 0;                                                 //当前状态
@@ -16,7 +16,6 @@ int color = G;                                                //亮灯的颜色
 
 //全局对象的构建
 LiquidCrystal LCD(rs, en, d4, d5, d6, d7); //LCD对象
-SoftwareSerial softserial(7, 6);           //软串口接入
 
 //初始化函数
 void setup()
@@ -49,15 +48,27 @@ void loop()
 {
     if (flag == 0) //复位状态
     {
-        if (millis() > set_time)
-        {
-            flag = 1;
-            digitalWrite(color, HIGH);
-            spend_time = 0;
-            Timer1.attachInterrupt(Isr1);
+    again:
+        flag = 0;
+        LCD.clear();
+        LCD.setCursor(0, 0);
+        LCD.print("Start!");
 
-            Serial.print("Yes!\n");
+        set_time = random(3000, 5000);
+        digitalWrite(color, LOW);
+        color = random(3, 5);
+        for (int i = 0; i < set_time / 10; i++)
+        {
+            delay(10);
+            vertify();
+            if (flag)
+                goto again;
         }
+
+        flag = 1;
+        digitalWrite(color, HIGH);
+        spend_time = 0;
+        Timer1.attachInterrupt(Isr1);
     }
     else if (flag == 1) //游戏进行状态
     {
@@ -67,45 +78,22 @@ void loop()
         LCD.clear();
         LCD.setCursor(0, 0);
         LCD.print(String(spend_time) + "ms");
+        for (int i = 0; i < 500 && flag == 2; i++)
+            delay(10);
+
+        flag = 0;
     }
 
-    int x = analogRead(Rx);
-    int y = analogRead(Ry);
-    if ((x > 800 || x < 200 || y > 800 || y < 200) && flag != 2) //摇杆被推动
-    {
-        digitalWrite(color, LOW); //指示灯熄灭
-        digitalWrite(R, HIGH);    //立刻亮红灯
-
-        if (flag == 0) //未亮灯之前已结束游戏
-            send_message(3);
-        else if (flag == 1) //结束游戏
-        {
-            if (color == G && y > 800 || color == B && x > 800) //正确推动
-            {
-                send_message(1);
-                flag = 2;
-            }
-            else //错误推动
-                send_message(2);
-        }
-        Timer1.detachInterrupt();
-
-        delay(500);
-        digitalWrite(R, LOW);
-    }
+    vertify();
 }
 
 //定时中断程序
 void Isr1()
 {
-
+    spend_time += 10;
     LCD.clear();
     LCD.setCursor(0, 0);
-    spend_time += 10;
     LCD.print(String(spend_time) + "ms");
-
-    Serial.print("flag = " + String(flag));
-    Serial.print("    set_time = " + String(set_time) + "\n");
 }
 
 //外界触发中断程序
@@ -113,12 +101,6 @@ void Isr2()
 {
     Timer1.detachInterrupt();
     flag = 0;
-    set_time = millis() + random(3000, 5000);
-    spend_time = 0;
-    digitalWrite(color, LOW);
-    color = random(3, 5);
-
-    Serial.print("No!");
 }
 
 //发送信息
@@ -127,16 +109,42 @@ void send_message(int meg)
     switch (meg)
     {
     case 1:
-        softserial.print("Your action time is" + String(spend_time) + "ms!");
+        Serial.print("Your action time is " + String(spend_time) + " ms!" + "\n");
         break;
     case 2:
-        softserial.print("You moved the rocker in a wrong direction!");
+        Serial.print("You moved the rocker in a wrong direction!\n");
         break;
     case 3:
-        softserial.print("LED hasn't been lighted!");
+        Serial.print("LED hasn't been lighted!\n");
         break;
     default:
-        softserial.print("error!");
+        Serial.print("error!\n");
         break;
+    }
+}
+
+void vertify()
+{
+    int x = analogRead(Rx);
+    int y = analogRead(Ry);
+    if ((x > 800 || x < 200 || y > 800 || y < 200) && flag != 2) //摇杆被推动
+    {
+        digitalWrite(color, LOW); //指示灯熄灭
+        digitalWrite(R, HIGH);    //立刻亮红灯
+        Timer1.detachInterrupt();
+
+        if (flag == 0) //未亮灯之前已结束游戏
+            send_message(3);
+        else if (flag == 1) //结束游戏
+        {
+            if (color == G && y > 800 || color == B && x > 800) //正确推动
+                send_message(1);
+            else //错误推动
+                send_message(2);
+        }
+
+        flag = 2;
+        delay(500);
+        digitalWrite(R, LOW);
     }
 }
