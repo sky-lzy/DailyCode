e38d3403d02452f69d5e5bf46c339b40ca2514b1
sky-lzy <289751894@qq.com>
2021-09-12 09:37:18 +0800

    master

---
+++ Arduino/Homework/Final_work/Final_1.ino
@@ -0,0 +1,173 @@
+/*******************************************
+ * 开发环境：Arduino UNO
+ * 模块：电机、LD3320语音识别（连线情况见代码）
+ * 功能：实现PID控制电机、语音识别功能、板间通信
+ * 编号：1
+ * ***************************************** */
+
+#include "ld3320.h"
+#include <IRremote.h>
+
+//引脚接口声明
+//语音需要引脚: MI D12; MO D11; SCK D13; CS D4; RST; D9; IRQ D2; WR GND
+#define ENA_1 3
+#define ENA_2 5
+#define STBY_1 6
+#define STBY_2 7
+#define IN1_1 A0
+#define IN2_1 A1
+#define IN1_2 A2
+#define IN2_2 A3
+
+//运动模式
+#define LEFT 1
+#define RIGHT 2
+#define FORWARD 3
+#define BACK 4
+#define STOP 0
+
+//对象实例化
+VoiceRecognition Voice; //声明一个语音识别对象
+
+//全局变量定义
+int mode = STOP;	   //运动模式
+
+//语音识别函数
+void LD3320_work()
+{
+	int temp = Voice.read();
+	if (temp != -1) //判断是否成功接受信息
+		mode = temp;
+}
+
+//运动状态：左转
+void turn_left()
+{
+	digitalWrite(IN1_1, HIGH);
+	digitalWrite(IN2_1, LOW);
+	digitalWrite(IN1_2, HIGH);
+	digitalWrite(IN2_2, LOW);
+
+	analogWrite(ENA_1, 25);
+	analogWrite(ENA_2, 100);
+	delay(25);
+}
+
+//运动状态：右转
+void turn_right()
+{
+	digitalWrite(IN1_1, HIGH);
+	digitalWrite(IN2_1, LOW);
+	digitalWrite(IN1_2, HIGH);
+	digitalWrite(IN2_2, LOW);
+
+	analogWrite(ENA_1, 100);
+	analogWrite(ENA_2, 25);
+	delay(25);
+}
+
+//运动状态：前进
+void move_forward()
+{
+	digitalWrite(IN1_1, HIGH);
+	digitalWrite(IN2_1, LOW);
+	digitalWrite(IN1_2, HIGH);
+	digitalWrite(IN2_2, LOW);
+
+	analogWrite(ENA_1, 50);
+	analogWrite(ENA_2, 50);
+	delay(50);
+}
+
+//运动状态: 后退
+void move_back()
+{
+	digitalWrite(IN1_1, LOW);
+	digitalWrite(IN2_1, HIGH);
+	digitalWrite(IN1_2, LOW);
+	digitalWrite(IN2_2, HIGH);
+
+	analogWrite(ENA_1, 50);
+	analogWrite(ENA_2, 50);
+	delay(50);
+}
+
+//初始化函数
+void setup()
+{
+	//串口波特率设置
+	Serial.begin(9600);
+
+	//初始化VoiceRecognition模块
+	Voice.init();
+	Voice.addCommand("zuo", LEFT);	   //添加左转指令
+	Voice.addCommand("you", RIGHT);	   //添加右转指令
+	Voice.addCommand("qian", FORWARD); //添加前进指令
+	Voice.addCommand("dao", BACK);	   //添加后退指令
+	Voice.addCommand("yi", STOP);	   //添加停止指令
+	Voice.start();					   //开始识别
+
+	//初始化引脚
+	pinMode(STBY_1, OUTPUT);
+	pinMode(STBY_2, OUTPUT);
+	pinMode(IN1_1, OUTPUT);
+	pinMode(IN1_2, OUTPUT);
+	pinMode(IN2_1, OUTPUT);
+	pinMode(IN2_2, OUTPUT);
+	pinMode(ENA_1, OUTPUT);
+	pinMode(ENA_2, OUTPUT);
+	digitalWrite(STBY_1, HIGH);
+	digitalWrite(STBY_2, HIGH);
+	digitalWrite(IN1_1, HIGH);
+	digitalWrite(IN2_1, LOW);
+	digitalWrite(IN1_2, HIGH);
+	digitalWrite(IN2_2, LOW);
+}
+
+void loop()
+{
+	LD3320_work();
+
+	switch (mode)
+	{
+	case LEFT:
+		turn_left();
+		// Serial.print("Turn Left!");
+		break;
+
+	case RIGHT:
+		turn_right();
+		// Serial.print("Turn Right!");
+		break;
+
+	case FORWARD:
+		// move_forward();
+		move_forward();
+		// Serial.print("hello\n");
+		// Serial.print("Forward!\n");
+		break;
+
+	case BACK:
+		move_back();
+		break;
+
+	case STOP:
+		analogWrite(ENA_1, 0);
+		analogWrite(ENA_2, 0);
+		delay(50);
+		// Serial.print("Stop!\n");
+		break;
+	default:
+		break;
+	}
+	// delay(100);
+	/* analogWrite(ENA_1, 100);
+	analogWrite(ENA_2, 100);
+	delay(1000);
+	analogWrite(ENA_1, 0);
+	analogWrite(ENA_2, 0);
+	delay(1000); */
+
+	//延时保持稳定
+	// delay(25);
+}
---
+++ Arduino/Homework/Final_work/Final_3.ino
@@ -0,0 +1,237 @@
+#include <IRremote.h>
+#include <Servo.h>
+
+#define RECV_PIN 13 //定义红外接收器的引脚为13
+#define RED_1 2
+#define GREEN_1 3
+#define BLUE_1 4
+#define RED_2 5
+#define GREEN_2 6
+#define BLUE_2 7
+#define Servo_1 8
+#define Servo_2 9
+
+//全局对象的定义
+IRrecv irrecv(RECV_PIN);
+decode_results results; //解码结果放在 decode results结构的 result中
+Servo servo_1;
+Servo servo_2;
+
+// int pos_1 = 0;
+// int pos_2 = 0;
+bool mode_1 = 0;
+bool mode_2 = 0;
+bool mode_3 = 0;
+bool mode_4 = 0;
+bool mode_5 = 0;
+bool mode_6 = 0;
+
+//熄灭所有灯
+void LED_setoff()
+{
+	digitalWrite(RED_1, LOW);
+	digitalWrite(GREEN_1, LOW);
+	digitalWrite(BLUE_1, LOW);
+	digitalWrite(RED_2, LOW);
+	digitalWrite(GREEN_2, LOW);
+	digitalWrite(BLUE_2, LOW);
+}
+
+//舵机一：向上移动
+void moveup()
+{
+	LED_setoff();
+	digitalWrite(RED_1, HIGH);
+	digitalWrite(GREEN_2, HIGH);
+	servo_1.writeMicroseconds(1400);
+	bool flag = true;
+	while (flag)
+	{
+		if (irrecv.decode(&results))
+		{
+			if (results.value == 0xFF38C7)
+				flag = false;
+			irrecv.resume();
+		}
+		delay(50);
+	}
+	servo_1.writeMicroseconds(1500);
+	digitalWrite(RED_1, LOW);
+	digitalWrite(GREEN_1, HIGH);
+}
+
+//舵机一：向下移动
+void movedown()
+{
+	LED_setoff();
+	digitalWrite(BLUE_1, HIGH);
+	digitalWrite(GREEN_2, HIGH);
+	servo_1.writeMicroseconds(1600);
+	bool flag = true;
+	while (flag)
+	{
+		if (irrecv.decode(&results))
+		{
+			if (results.value == 0xFF38C7)
+				flag = false;
+			irrecv.resume();
+		}
+		delay(50);
+	}
+	servo_1.writeMicroseconds(1500);
+	digitalWrite(BLUE_1, LOW);
+	digitalWrite(GREEN_1, HIGH);
+}
+
+//舵机二：开口打开
+void moveleft()
+{
+	LED_setoff();
+	digitalWrite(RED_2, HIGH);
+	digitalWrite(GREEN_1, HIGH);
+	bool flag = true;
+	while (flag)
+	{
+		servo_2.writeMicroseconds(1400);
+		delay(50);
+		if (irrecv.decode(&results))
+		{
+			if (results.value == 0xFF38C7)
+				flag = false;
+			irrecv.resume();
+		}
+	}
+	servo_2.writeMicroseconds(1500);
+	digitalWrite(RED_2, LOW);
+	digitalWrite(GREEN_2, HIGH);
+}
+
+//舵机二：开口闭合
+void moveright()
+{
+	LED_setoff();
+	digitalWrite(BLUE_2, HIGH);
+	digitalWrite(GREEN_1, HIGH);
+	bool flag = true;
+	while (flag)
+	{
+		servo_2.writeMicroseconds(1600);
+		delay(50);
+		if (irrecv.decode(&results))
+		{
+			if (results.value == 0xFF38C7)
+				flag = false;
+			irrecv.resume();
+		}
+	}
+	servo_2.writeMicroseconds(1500);
+	digitalWrite(BLUE_2, LOW);
+	digitalWrite(GREEN_2, HIGH);
+}
+
+void setup()
+{
+	Serial.begin(9600);
+
+	//启动接收器
+	irrecv.enableIRIn();
+
+	//初始化舵机
+	servo_1.attach(Servo_1);
+	servo_2.attach(Servo_2);
+	// servo_1.write(pos_1);
+	// servo_2.write(pos_2);
+
+	//初始化引脚
+	pinMode(RED_1, OUTPUT);
+	pinMode(GREEN_1, OUTPUT);
+	pinMode(BLUE_1, OUTPUT);
+	pinMode(RED_2, OUTPUT);
+	pinMode(GREEN_2, OUTPUT);
+	pinMode(BLUE_2, OUTPUT);
+	LED_setoff();
+	digitalWrite(GREEN_1, HIGH);
+	digitalWrite(GREEN_2, HIGH);
+}
+
+void loop()
+{
+	if (irrecv.decode(&results)) //解码成功，收到一组红外讯号
+	{
+		switch (results.value)
+		{
+		case 0xFFA25D:
+			mode_1 = !mode_1;
+			digitalWrite(RED_1, mode_1);
+			break;
+
+		case 0xFF629D:
+			mode_2 = !mode_2;
+			digitalWrite(GREEN_1, mode_2);
+			break;
+
+		case 0xFFE21D:
+			mode_3 = !mode_3;
+			digitalWrite(BLUE_1, mode_3);
+			break;
+
+		case 0xFF22DD:
+			mode_4 = !mode_4;
+			digitalWrite(RED_2, mode_4);
+			break;
+
+		case 0xFF02FD:
+			mode_5 = !mode_5;
+			digitalWrite(GREEN_2, mode_5);
+			break;
+
+		case 0xFFC23D:
+			mode_6 = !mode_6;
+			digitalWrite(BLUE_2, mode_6);
+			break;
+
+		case 0xFF38C7:
+			break;
+
+		case 0xFF18E7:
+			digitalWrite(RED_2, HIGH);
+			moveup();
+			break;
+
+		case 0xFF4AB5:
+			digitalWrite(RED_2, HIGH);
+			movedown();
+			break;
+
+		case 0xFF10EF:
+			digitalWrite(RED_2, HIGH);
+			moveleft();
+			break;
+
+		case 0xFF5AA5:
+			digitalWrite(RED_2, HIGH);
+			moveright();
+			break;
+
+		default:
+			break;
+		}
+		irrecv.resume(); // 接收下一个值
+	}
+	delay(100);
+}
+
+/* #include <Servo.h>
+Servo myservo;
+void setup()
+{
+	myservo.attach(8);
+}
+
+void loop()
+{
+	myservo.writeMicroseconds(1400); // 舵机顺时针旋转
+	delay(2000);
+	myservo.writeMicroseconds(1500); // 舵机逆时针旋转
+	delay(2000);
+} */
