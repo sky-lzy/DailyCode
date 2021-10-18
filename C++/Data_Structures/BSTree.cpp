ed7ca6774a2399327862331bc18552a082b63812
sky-lzy <289751894@qq.com>
2021-10-18 20:48:10 +0800

    BSTree方法扩充

--- C++/Data_Structures/BSTree.cpp
+++ C++/Data_Structures/BSTree.cpp
@@ -26,20 +26,27 @@ struct BSTree
     Node *Search(Node *r, int k) const; //查找
     Node *Maximum(Node *r) const;       //最大结点
     Node *Minimum(Node *r) const;       //最小结点
-    void Inorder_walk(Node *r) const;   //中序遍历
     Node *Successor(Node *r) const;     //后继
     Node *Predecessor(Node *r) const;   //前驱
+    void Print() const;                 //按序打印信息
 
     //修改操作
-    void Insert(int k);                 //插入
-    void Delete(Node *r);               //删除
+    void Insert(int k);   //插入
+    void Delete(Node *r); //删除
 
     //树的构建
     void Build(int *keys, int n);
+
+//内部实现方法
+private:
+    void Transplant(Node *r1, Node *r2); //移植子树
+    void Inorder_walk(Node *r) const;    //中序遍历
+    void DeleteAll(Node *r);             //删除子树
 };
 
 BSTree::~BSTree()
 {
+    DeleteAll(root);
 }
 
 void BSTree::Build(int *keys, int n)
@@ -77,7 +84,7 @@ void BSTree::Inorder_walk(Node *r) const
     if (r != nullptr)
     {
         Inorder_walk(r->left);
-        std::cout << r->key;
+        std::cout << r->key << " ";
         Inorder_walk(r->right);
     }
 }
@@ -135,3 +142,70 @@ void BSTree::Insert(int k)
     }
 }
 
+void BSTree::Delete(Node *r)
+{
+    if (r->left == nullptr)
+        Transplant(r->right, r);
+    else if (r->right == nullptr)
+        Transplant(r->left, r);
+    else
+    {
+        Node *y = Minimum(r->right);
+        if (y->parent != r)
+        {
+            Transplant(y->right, y);
+            y->right = r->right;
+            y->right->parent = y;
+        }
+
+        Transplant(y, r);
+        y->left = r->left;
+        y->left->parent = y;
+    }
+    delete r;
+}
+
+void BSTree::DeleteAll(Node *r)
+{
+    if (r != nullptr)
+    {
+        if (!r && r == r->parent->left)
+            r->parent->left = nullptr;
+        else if (!r && r == r->parent->right)
+            r->parent->right = nullptr;
+        DeleteAll(r->left);
+        DeleteAll(r->right);
+        delete r;
+    }
+}
+
+void BSTree::Transplant(Node *r1, Node *r2)
+{
+    if (r2->parent == nullptr)
+        root = r1;
+    else if (r2 == r2->parent->left)
+        r2->parent->left = r1;
+    else
+        r2->parent->right = r1;
+    if (r1 != nullptr)
+        r1->parent = r2->parent;
+}
+
+void BSTree::Print() const
+{
+    Inorder_walk(root);
+}
+
+//测试代码
+int main()
+{
+    int a[10] = {3, 8, 9, 6, 1, 0, 5, 2, 7, 4};
+    BSTree tree;
+    tree.Build(a, 10);
+    tree.Insert(10);
+    tree.Delete(tree.Search(tree.root, 5));
+    tree.Delete(tree.Search(tree.root, 3));
+    tree.Delete(tree.Search(tree.root, 8));
+    tree.Print();
+    return 0;
+}
