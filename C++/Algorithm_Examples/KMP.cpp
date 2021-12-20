6f023eb8733eb265041370d7608638d89383db4e
sky-lzy <289751894@qq.com>
2021-12-20 21:48:04 +0800

    KMP

--- C++/Algorithm_Examples/KMP.cpp
+++ C++/Algorithm_Examples/KMP.cpp
@@ -1,35 +1,78 @@
-/********************
- * KMP算法  字符串匹配
- * 复杂度: Θ(m + n)
- * ****************** */
-//未完成
-
 #include <iostream>
 #include <vector>
+#include <string>
 
-std::vector<int> Pretreatment(std::string &test)
+void PreWork(const std::string &str, std::vector<int> &Pre)
 {
-    int m = test.size();
-    std::vector<int> Next({0});
-    for (int i = 1; i < m; i++)
+    int size = str.size();
+    // Pre.swap(std::vector<int>(size, 0));
+    Pre.resize(size);
+    Pre[0] = 0;
+    int i = 1, j = 0; //i为Pre[i]，j为待比较的str[j]
+    while (i < size)
     {
-        int k = m - 1;
-        for (; k > 0; k--)
+        //匹配
+        if (str[i] == str[j])
         {
-            int j = 0;
-            for (; j <= k; j++)
-            {
-                if (test[j] != test[m - k + j])
-                    break;
-            }
-            if (j == k + 1)
-                break;
+            Pre[i] = j + 1;
+            i++;
+            j++;
         }
-        Next.emplace_back(k);
+        //若不匹配，则回退至已经比较过的
+        else if (j > 0)
+            j = Pre[j - 1];
+        //若始终不匹配，则无匹配串
+        else
+        {
+            Pre[i] = 0;
+            i++;
+        }
     }
 }
 
-int KMP(std::string &String, std::string &test)
+int KMP(std::string &str1, std::string &str2)
 {
-    int n = String.size(), m = test.size();
+    std::vector<int> Pre;
+    PreWork(str2, Pre);
+    int size1 = str1.size(), size2 = str2.size();
+
+    int i = 0, j = 0;
+    while (i < size1)
+    {
+        if (str1[i] == str2[j])
+        {
+            //是否已经比较完子串
+            if (j == size2 - 1)
+                return i - size2 + 1;
+            else
+            {
+                i++;
+                j++;
+            }
+        }
+        else
+        {
+            if (j > 0)
+                j = Pre[j - 1];
+            else
+                i++;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    std::string str1, str2;
+    std::cout << "input str1:\n";
+    std::cin >> str1;
+    std::cout << "input str2:\n";
+    std::cin >> str2;
+    int match = KMP(str1, str2);
+    if (match == -1)
+        std::cout << "No Matching!\n";
+    else
+        std::cout << match << '\n';
+
+    return 0;
 }
