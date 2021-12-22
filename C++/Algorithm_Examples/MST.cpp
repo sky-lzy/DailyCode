e6c755cba0a0f4b55862df31348192aa38612bd5
sky-lzy <289751894@qq.com>
2021-12-22 21:36:41 +0800

    MST_Prim

--- C++/Algorithm_Examples/MST.cpp
+++ C++/Algorithm_Examples/MST.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <algorithm>
 #include <utility>
 #include <vector>
+#include <tuple>
 #include <queue>
 
 class Graph
@@ -29,17 +31,34 @@ Graph::Graph(int inputNodeNum, int inputEdgeNum) : NodeNum(inputNodeNum), EdgeNu
     }
 }
 
-struct CompairEdge
-{
-    bool operator()(const std::pair<int, int> &p1, const std::pair<int, int> &p2) { return p1.second > p2.second; }
-};
-
 void Graph::Prim() const
 {
     std::vector<bool> tempNode(NodeNum, false);
-    std::vector<std::pair<int, int>> tempEdge;
+    std::vector<std::tuple<int, int, int>> tempMST;
     tempNode[0] = true;
-    while (tempEdge.size() < NodeNum)
+    for (int i = 0; i < NodeNum - 1; i++)
     {
+        std::tuple<int, int, int> tempEdge(0, 0, 0x7fffffff);
+        for (int j = 0; j < NodeNum; j++)
+            if (tempNode[j])
+                std::for_each(EdgeList[j].begin(), EdgeList[j].end(), [&tempNode, &tempEdge, &j](const std::pair<int, int> &t) -> void
+                              {
+                                  if (!tempNode[t.first] && t.second < std::get<2>(tempEdge))
+                                      tempEdge = std::make_tuple(j, t.first, t.second);
+                              });
+        tempNode[std::get<1>(tempEdge)] = true;
+        tempMST.emplace_back(tempEdge);
     }
+
+    std::for_each(tempMST.begin(), tempMST.end(), [](const std::tuple<int, int, int> &t)
+                  { std::cout << std::get<0>(t) << " -> " << std::get<1>(t) << "  cost: " << std::get<2>(t) << "\n"; });
+}
+
+int main()
+{
+    int NodeNum, EdgeNum;
+    std::cin >> NodeNum >> EdgeNum;
+    Graph graph(NodeNum, EdgeNum);
+    graph.Prim();
+    return 0;
 }
