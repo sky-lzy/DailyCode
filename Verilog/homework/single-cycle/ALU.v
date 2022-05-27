b044826e015492bc30a283a44849004b1a3f47de
sky-lzy <289751894@qq.com>
2022-05-27 15:20:11 +0800

    数逻final2_question

---
+++ Verilog/homework/single-cycle/ALU.v
@@ -0,0 +1,15 @@
+
+module ALU(in1, in2, ALUCtrl, Sign, out, zero);
+	input [31:0] in1, in2;
+	input [4:0] ALUCtrl;
+	input Sign;
+	output reg [31:0] out;
+	output zero;
+	
+	// Your code below
+
+	// ...
+	     
+	// Your code above
+	
+endmodule
---
+++ Verilog/homework/single-cycle/ALUControl.v
@@ -0,0 +1,14 @@
+
+module ALUControl(OpCode, Funct, ALUCtrl, Sign);
+	input [5:0] OpCode;
+	input [5:0] Funct;
+	output reg [4:0] ALUCtrl;
+	output Sign;
+	
+	// Your code below
+
+	// ...
+	     
+	// Your code above
+
+endmodule
